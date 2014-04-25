d6a7e020ad22272afd32414f773a99246f27db57
Antoine <toine512@gmail.com>
2014-04-25 16:01:52 +0200

    Updates translation

--- fr.ts
+++ fr.ts
@@ -14,24 +14,24 @@
         <translation>Charger manuellement</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="119"/>
-        <location filename="mainwindow.cpp" line="189"/>
+        <location filename="mainwindow.cpp" line="120"/>
+        <location filename="mainwindow.cpp" line="190"/>
         <source>Apply</source>
         <translation>Appliquer</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="129"/>
+        <location filename="mainwindow.cpp" line="130"/>
         <source>Use as mood message</source>
         <translation>Utiliser comme message d&apos;humeur</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="136"/>
+        <location filename="mainwindow.cpp" line="137"/>
         <source>Remove from history</source>
         <translation>Retirer de l&apos;historique</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="141"/>
-        <location filename="mainwindow.cpp" line="467"/>
+        <location filename="mainwindow.cpp" line="142"/>
+        <location filename="mainwindow.cpp" line="449"/>
         <source>Clear history</source>
         <translation>Vider l&apos;historique</translation>
     </message>
@@ -46,103 +46,103 @@
         <translation>Il n&apos;y a aucun profil Skype pour cet utilisateur.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="167"/>
+        <location filename="mainwindow.cpp" line="168"/>
         <source>Mood message</source>
         <translation>Humeur</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="170"/>
+        <location filename="mainwindow.cpp" line="171"/>
         <source>History</source>
         <translation>Historique</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="185"/>
+        <location filename="mainwindow.cpp" line="186"/>
         <source>Apply and start Skype</source>
         <translation>Appliquer et démarrer Skype</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="200"/>
+        <location filename="mainwindow.cpp" line="201"/>
         <source>Close and start Skype</source>
         <translation>Fermer et démarrer Skype</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="204"/>
+        <location filename="mainwindow.cpp" line="205"/>
         <source>Close</source>
         <translation>Fermer</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="272"/>
+        <location filename="mainwindow.cpp" line="253"/>
         <source>Choose a Skype user&apos;s main.db</source>
         <translation>Choisissez la base de donnée d&apos;un utilisateur de Skype (main.db)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="272"/>
+        <location filename="mainwindow.cpp" line="253"/>
         <source>Skype user database (main.db);;Any file (*.*)</source>
         <translation>Base de données d&apos;utilisateur Skype (main.db);;Tous les fichiers (*.*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="299"/>
-        <location filename="mainwindow.cpp" line="326"/>
+        <location filename="mainwindow.cpp" line="280"/>
+        <location filename="mainwindow.cpp" line="307"/>
         <source>main.db locked</source>
         <translation>main.db est vérouillé</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="299"/>
+        <location filename="mainwindow.cpp" line="280"/>
         <source>The database is locked by Skype!</source>
         <translation>La base de données est vérouillée par Skype !</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="299"/>
+        <location filename="mainwindow.cpp" line="280"/>
         <source>Exit Skype manually</source>
         <translation>Quitter Skype manuellement</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="299"/>
+        <location filename="mainwindow.cpp" line="280"/>
         <source>Exit Skype for me</source>
         <translation>Quitter Skype automatiquement</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="311"/>
+        <location filename="mainwindow.cpp" line="292"/>
         <source>Waiting for Skype to release the lock on the database…</source>
         <translation>Attente de la libération du verrou de la base de données …</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="326"/>
+        <location filename="mainwindow.cpp" line="307"/>
         <source>Skype has not released the lock on the databse after 15 s.</source>
         <translation>Skype n&apos;a pas dévérouillé la base de données après 15 s.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="333"/>
+        <location filename="mainwindow.cpp" line="314"/>
         <source>Unable to terminate Skype.exe</source>
         <translation>Impossible de terminer Skype.exe</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="333"/>
+        <location filename="mainwindow.cpp" line="314"/>
         <source>Skype is not running or has not been terminated correctly (main.lock exists).</source>
         <translation>Skype n&apos;est pas en fonctionnement ou n&apos;a pas été arrêté correctement (main.lock existe).</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="404"/>
+        <location filename="mainwindow.cpp" line="386"/>
         <source>Reading query</source>
         <translation>Requête de lecture</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="404"/>
+        <location filename="mainwindow.cpp" line="386"/>
         <source>Cannot read current mood. Please report this issue.</source>
         <translation>Impossible de lire l&apos;humeur actuelle. Veuillez signaler cette erreur.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="409"/>
+        <location filename="mainwindow.cpp" line="391"/>
         <source>Error querying the database!</source>
         <translation>Erreur lors de la requête à la base de données !</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="414"/>
+        <location filename="mainwindow.cpp" line="396"/>
         <source>Error opening the database!</source>
         <translation>Erreur lors de l&apos;ouverture de la base de données !</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="467"/>
+        <location filename="mainwindow.cpp" line="449"/>
         <source>Are you sure you want to clear the history?</source>
         <translation>Voulez-vous vraiment vider l&apos;historique ?</translation>
     </message>
@@ -161,8 +161,9 @@
     <name>MenuDialog</name>
     <message>
         <location filename="menudialog.cpp" line="6"/>
-        <source>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;Skype Richtext Mood Editor v. %1&lt;/b&gt;&lt;br /&gt;Copyright © 2014 toine512&lt;br /&gt;Compiled on [%2] [%3]&lt;br /&gt;&lt;br /&gt;This software is distributed under &lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU General Public License v. 3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Written in C++ using &lt;a href=&quot;http://qt-project.org/&quot;&gt;Qt&lt;/a&gt; 5.2 (&lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU GPL v. 3&lt;/a&gt;).&lt;br /&gt;Uses &lt;a href=&quot;http://brsev.deviantart.com/gallery/#/d24gow2&quot;&gt;Token icon set&lt;/a&gt; by &lt;a href=&quot;http://brsev.com/&quot;&gt;Evan Brooks&lt;/a&gt;.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <source>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;Skype Richtext Mood Editor v. %1&lt;/b&gt;&lt;br /&gt;Copyright © 2014 toine512&lt;br /&gt;Compiled on [%2] [%3]&lt;br /&gt;&lt;br /&gt;This software is distributed under &lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU General Public License v. 3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Written in C++ using &lt;a href=&quot;http://qt-project.org/&quot;&gt;Qt&lt;/a&gt; 5.2 (&lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU GPL v. 3&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;Uses &lt;a href=&quot;http://brsev.deviantart.com/gallery/#/d24gow2&quot;&gt;Token icon set&lt;/a&gt; by &lt;a href=&quot;http://brsev.com/&quot;&gt;Evan Brooks&lt;/a&gt;,&lt;br /&gt;&lt;a href=&quot;https://dribbble.com/shots/1383631-ilepixeli-icons&quot;&gt;ilepixeli icons&lt;/a&gt; by Piotr Makarewicz (public domain),&lt;br /&gt;and &lt;a href=&quot;http://cgit.freedesktop.org/libreoffice/core/tree/icon-themes&quot;&gt;various icons&lt;/a&gt; from LibreOffice 4.3.&lt;/p&gt;</source>
+        <oldsource>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;Skype Richtext Mood Editor v. %1&lt;/b&gt;&lt;br /&gt;Copyright © 2014 toine512&lt;br /&gt;Compiled on [%2] [%3]&lt;br /&gt;&lt;br /&gt;This software is distributed under &lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU General Public License v. 3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Written in C++ using &lt;a href=&quot;http://qt-project.org/&quot;&gt;Qt&lt;/a&gt; 5.2 (&lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU GPL v. 3&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;Uses &lt;a href=&quot;http://brsev.deviantart.com/gallery/#/d24gow2&quot;&gt;Token icon set&lt;/a&gt; by &lt;a href=&quot;http://brsev.com/&quot;&gt;Evan Brooks&lt;/a&gt;,&lt;br /&gt;&lt;a href=&quot;https://dribbble.com/shots/1383631-ilepixeli-icons&quot;&gt;ilepixeli icons&lt;/a&gt; by Piotr Makarewicz (public domain),&lt;br /&gt;and &lt;a href=&quot;http://cgit.freedesktop.org/libreoffice/core/tree/icon-themes&quot;&gt;various icons&lt;/a&gt; from LibreOffice 4.2.&lt;/p&gt;</oldsource>
+        <translation>&lt;p align=&quot;center&quot;&gt;&lt;b&gt;Skype Richtext Mood Editor v. %1&lt;/b&gt;&lt;br /&gt;Copyright © 2014 toine512&lt;br /&gt;Compilé le [%2] à [%3]&lt;br /&gt;&lt;br /&gt;ce logiciel est distribué sous les termes de la &lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU General Public License v. 3&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Écrit en C++ avec &lt;a href=&quot;http://qt-project.org/&quot;&gt;Qt&lt;/a&gt; 5.2 (&lt;a href=&quot;https://www.gnu.org/licenses/gpl.html&quot;&gt;GNU GPL v. 3&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;Utilise &lt;a href=&quot;http://brsev.deviantart.com/gallery/#/d24gow2&quot;&gt;Token icon set&lt;/a&gt; par &lt;a href=&quot;http://brsev.com/&quot;&gt;Evan Brooks&lt;/a&gt;,&lt;br /&gt;&lt;a href=&quot;https://dribbble.com/shots/1383631-ilepixeli-icons&quot;&gt;ilepixeli icons&lt;/a&gt; par Piotr Makarewicz (domaine public),&lt;br /&gt;et &lt;a href=&quot;http://cgit.freedesktop.org/libreoffice/core/tree/icon-themes&quot;&gt;différentes icônes&lt;/a&gt; issues de LibreOffice 4.3.&lt;/p&gt;</translation>
     </message>
     <message>
         <location filename="menudialog.cpp" line="12"/>
@@ -175,22 +176,27 @@
         <translation>Fermer</translation>
     </message>
     <message>
-        <location filename="menudialog.cpp" line="25"/>
+        <location filename="menudialog.cpp" line="32"/>
+        <source>Parameters</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="menudialog.cpp" line="33"/>
         <source>Language:</source>
         <translation>Langue :</translation>
     </message>
     <message>
-        <location filename="menudialog.cpp" line="32"/>
+        <location filename="menudialog.cpp" line="41"/>
         <source>Menu</source>
         <translation>Menu</translation>
     </message>
     <message>
-        <location filename="menudialog.cpp" line="38"/>
+        <location filename="menudialog.cpp" line="47"/>
         <source>Translation</source>
         <translation>Traduction</translation>
     </message>
     <message>
-        <location filename="menudialog.cpp" line="38"/>
+        <location filename="menudialog.cpp" line="47"/>
         <source>Please restart the application for the change to take effect.</source>
         <translation>Veuillez redémarrer l&apos;application pour que le changement prenne effet.</translation>
     </message>
@@ -248,17 +254,17 @@
         <translation>Couleur du texte</translation>
     </message>
     <message>
-        <location filename="tagsplaintextedit.cpp" line="88"/>
+        <location filename="tagsplaintextedit.cpp" line="133"/>
         <source>Hypertext link target</source>
         <translation>Cible du lien hypertexte</translation>
     </message>
     <message>
-        <location filename="tagsplaintextedit.cpp" line="88"/>
+        <location filename="tagsplaintextedit.cpp" line="133"/>
         <source>URL:</source>
         <translation>URL :</translation>
     </message>
     <message>
-        <location filename="tagsplaintextedit.cpp" line="124"/>
+        <location filename="tagsplaintextedit.cpp" line="169"/>
         <source>Font color</source>
         <translation>Couleur du texte</translation>
     </message>
@@ -267,8 +273,8 @@
     <name>meta</name>
     <message>
         <location filename="mainwindow.cpp" line="69"/>
-        <location filename="menudialog.cpp" line="57"/>
-        <location filename="menudialog.cpp" line="59"/>
+        <location filename="menudialog.cpp" line="66"/>
+        <location filename="menudialog.cpp" line="68"/>
         <source>language</source>
         <translation>Français</translation>
     </message>

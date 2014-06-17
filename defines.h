65aaf62bbebc1ad77bbdddfabce3c6b5c33d91ba
Antoine <toine512@gmail.com>
2014-06-17 19:16:35 +0200

    1.0 Alpha

--- defines.h
+++ defines.h
@@ -5,10 +5,10 @@
    0x 00 00 00 0A = alpha
    0x 00 00 00 0B = beta
    0x 00 00 00 0F = release */
-#define SKYPE_MOOD_VERSION 0x0009000A //uint32_t For The Win !
+#define SKYPE_MOOD_VERSION 0x0100000A //uint32_t For The Win !
 
 //Human readable version
-#define SKYPE_MOOD_VERSION_HUMAN "0.9"
+#define SKYPE_MOOD_VERSION_HUMAN "1.0"
 
 //Skype database lock timeout
 #define MAINDB_LOCK_TIMEOUT 15
